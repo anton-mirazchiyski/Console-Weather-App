0b4620459a2bab4d9a7557e830edec0b81aebc64
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-11-02 23:52:03 +0200

    Function to get city coords from API

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_key = os.getenv('WEATHER_API_KEY')
+
+
 def validate_user_input(city_name):
     return city_name.isalpha()
 
@@ -8,8 +18,28 @@ def get_user_input(text='Enter city name: '):
     return get_user_input(text='Enter a valid city name: ') if not result else city_name
 
 
+def get_city_coordinates_from_city_name(city_name):
+    url = 'http://api.openweathermap.org/geo/1.0/direct'
+    params_dict = {'q': city_name, 'appid': API_key,}
+
+    response = requests.get(url, params=params_dict)
+    data = response.json()
+    latitude, longitude = None, None
+
+    for element in data:
+        for key, value in element.items():
+            if key == 'lat':
+                latitude = value
+            if key == 'lon':
+                longitude = value
+                break
+
+    return latitude, longitude
+
 def main():
     user_input = get_user_input()
+    city_name = user_input.upper()
+    latitude, longitude = get_city_coordinates_from_city_name(city_name)
 
 
 if __name__ == '__main__':
