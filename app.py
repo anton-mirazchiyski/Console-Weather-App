b823f1ceddfe57289082176d3b32a9bc28f18151
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2024-11-02 20:09:06 +0200

    Functions to get and validate input from user

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+def validate_user_input(city_name):
+    return city_name.isalpha()
+
+
+def get_user_input(text='Enter city name: '):
+    city_name = input(text)
+    result = validate_user_input(city_name)
+    return get_user_input(text='Enter a valid city name: ') if not result else city_name
+
+
 def main():
-    pass
+    user_input = get_user_input()
 
 
 if __name__ == '__main__':
